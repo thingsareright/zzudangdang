c91f317951df4e0114af7b688656b92ccee810fd
GaoJiuJiu <929226915@qq.com>
2017-12-04 14:55:14 +0800

    只添加了搜索后界面上半部分的按钮，未添加书列表的那部分，且还没添加监听事件

---
+++ app/src/main/java/com/example/administrator/zzudangdang/GaoJiujiu/BookList_Activity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.zzudangdang.GaoJiujiu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.zzudangdang.R;
+
+public class BookList_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+    }
+}
