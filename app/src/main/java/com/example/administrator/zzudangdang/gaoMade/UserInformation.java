0cf105968e877cff7bbc8184c1302d879aea1093
GaoJiuJiu <929226915@qq.com>
2017-12-20 10:11:32 +0800

    gaojiujiu12_20

---
+++ app/src/main/java/com/example/administrator/zzudangdang/gaoMade/UserInformation.java
@@ -0,0 +1,22 @@
+package com.example.administrator.zzudangdang.gaoMade;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.administrator.zzudangdang.R;
+
+
+public class UserInformation extends AppCompatActivity {
+    //这只一个toolbar，单位显示上面的文字
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+       // Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+    }
+
+
+
+}
