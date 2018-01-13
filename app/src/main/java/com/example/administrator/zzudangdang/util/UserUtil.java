6eb6a97fe88acbf4c09f81f643ebe11af30dddd9
thingsareright <2271366490@qq.com>
2018-01-13 20:08:19 +0800

    解决了记住密码和用户个人界面头像显示不出来的问题。

--- app/src/main/java/com/example/administrator/zzudangdang/util/UserUtil.java
+++ app/src/main/java/com/example/administrator/zzudangdang/util/UserUtil.java
@@ -1,6 +1,18 @@
 package com.example.administrator.zzudangdang.util;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.anupcowkur.reservoir.Reservoir;
+import com.anupcowkur.reservoir.ReservoirPutCallback;
+import com.example.administrator.zzudangdang.activity.Home_Activity;
+import com.example.administrator.zzudangdang.activity.Login_Activity;
 import com.example.administrator.zzudangdang.dao.entity.User;
+
+import java.io.IOException;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by Administrator on 2017/12/30 0030.
@@ -11,10 +23,40 @@ public class UserUtil {
     private static User user =null;
 
     public static User getOnlyUser() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(MyApplication.getContext());
+        User user = new User();
+
+        if (!sp.contains("phone")){
+            Intent intent = new Intent(MyApplication.getContext(), Login_Activity.class);
+            MyApplication.getContext().startActivity(intent);
+        }
+        user.setPhone(sp.getString("phone","18838951998"));
+        user.setPassword(sp.getString("password","123"));
+        user.setHead(sp.getString("headuri","yinlin.jpg"));
         return user;
     }
 
-    public static void setOnlyUser(User user1) {
-        user = user1;
+    public static void setOnlyUser(final User user1)  {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(MyApplication.getContext());
+        SharedPreferences.Editor editor = sp.edit();
+        if (sp.contains("phone")) {
+            editor.remove("phone");
+            editor.commit();
+        }
+        if (sp.contains("password")){
+            editor.remove("password");
+            editor.commit();
+        }
+        if (sp.contains("headuri")) {
+            editor.remove("headuri");
+            editor.commit();
+        }
+        editor.putString("phone", user1.getPhone());
+        editor.putString("password", user1.getPassword());
+        editor.putString("headuri", user1.getHead());
+        editor.commit();
+        editor.apply();
     }
+
+
 }
